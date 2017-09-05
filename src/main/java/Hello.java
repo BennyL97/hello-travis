623dc99609443142b95137d1142c9aa055bf8623
Benjamin Lutz <Lutz@STUDENTS>
2017-09-05 14:55:19 +0200

    Name, Ausgbe des Namens

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,17 +1,17 @@
 
 /**
  * A class containing a main method which prints "Hello Travis!"
- * @author Johannes Drönner
+ * @author Benjamin Lutz
  *
  */
 public class Hello {
 	
 	/**
 	 * A static method returning "Hello Travis!".
-	 * @return "Hello Travis!"
+	 * @return "Hello Benny!"
 	 */
-	static public String sayHelloTravis() {
-		return "Hello Travis ";
+	static public String sayHelloBenny() {
+		return "Hello Benny ";
 	}; 
 
 	/**
@@ -19,7 +19,7 @@ public class Hello {
 	 * @param args not used
 	 */
 	public static void main(String[] args) {
-		System.out.println(sayHelloTravis()+"!");
+		System.out.println(sayHelloBenny()+"!");
 
 	}
 
